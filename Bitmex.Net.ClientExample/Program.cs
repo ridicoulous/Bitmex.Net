4a1115dcdda71420e5dda9bae98b2739684f316e
Артем Курьянов <artemkuryanov@gmail.com>
2020-03-04 09:42:07 +0200

    moved keys to config

--- Bitmex.Net.ClientExample/Program.cs
+++ Bitmex.Net.ClientExample/Program.cs
@@ -3,16 +3,28 @@ using Bitmex.Net.Client.Objects.Socket.Repsonses;
 using System;
 using System.Threading.Tasks;
 using Bitmex.Net.Client.Helpers.Extensions;
+using Microsoft.Extensions.Configuration;
+
 namespace Bitmex.Net.ClientExample
 {
     class Program
     {
         static async Task Main(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+           .AddJsonFile("appconfig.json", optional: true, reloadOnChange: true);
 
-            var socket = new BitmexSocketClient(new BitmexSocketClientOptions("W4kfhlp9OhwtHquxV0Zsd5WM", "TDvu6R2hfp3Si8gUrsiNuqYZ_llKW5p2CGFUyuJfOLEjM0it",true));
+           var configuration = builder.Build();
+            var t = configuration["key"];
+            var socket = new BitmexSocketClient(new BitmexSocketClientOptions(configuration["key"], configuration["secret"], bool.Parse(configuration["testnet"])));
             socket.SubscribeToUserExecutions(Exec, "XBTUSD");
+            socket.SubscribeToUserOrderUpdates(Exec, "XBTUSD");
             Console.ReadLine();
+        }
+
+        private static void Exec(BitmexOrderUpdateEvent obj)
+        {
+          
         }
 
         private static void Exec(BitmexExecutionEvent obj)
