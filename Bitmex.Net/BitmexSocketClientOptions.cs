eda88dd6d69b56ad31f429341bbbd807f86d7fc9
alokym86 <alokym86@gmail.com>
2022-11-25 18:13:03 +0200

    reorganize ctors and props

--- Bitmex.Net/BitmexSocketClientOptions.cs
+++ Bitmex.Net/BitmexSocketClientOptions.cs
@@ -14,24 +14,37 @@ namespace Bitmex.Net.Client
         public bool SendPingManually = false;
 
         
-        public BitmexSocketClientOptions() : this(false, false, true)
+        public BitmexSocketClientOptions() : this(false, true)
         {
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="secret"></param>
+        /// <param name="isTestnet"></param>
+        /// <param name=""></param>
+        /// <param name="loadInstrumentIndexes">If you will subscribe to orderbook, set it to true, cause instrument index and tick size will be used for price calculation</param>
+        /// <param name="useMultiplexing"></param>
+        public BitmexSocketClientOptions(string key, string secret, bool isTestnet = false, bool loadInstrumentIndexes = true) : this(isTestnet, loadInstrumentIndexes)
+        {
+            key.ValidateNotNull(nameof(key));
+            secret.ValidateNotNull(nameof(secret));
+            ApiCredentials = new CryptoExchange.Net.Authentication.ApiCredentials(key, secret);
+        }
+
         /// <summary>
         /// 
         /// </summary>
         /// <param name="isTestnet"></param>
         /// <param name="useMultiplexing"></param>
         /// <param name="loadInstrumentIndexes">If you will subscribe to orderbook, set it to true, cause instrument index and tick size will be used for price calculation</param>
-        public BitmexSocketClientOptions(bool isTestnet, bool useMultiplexing = false, bool loadInstrumentIndexes=true) : base()
+        public BitmexSocketClientOptions(bool isTestnet, bool loadInstrumentIndexes=true, SocketApiClientOptions commonStreamsOptions = null) : base()
         {
-            CommonStreamsOptions = new SocketApiClientOptions(isTestnet ? TestNetSocketEndpoint : SocketEndpoint);
+            CommonStreamsOptions = commonStreamsOptions ?? new SocketApiClientOptions(isTestnet ? TestNetSocketEndpoint : SocketEndpoint);
             LoadInstruments = loadInstrumentIndexes;
             IsTestnet = isTestnet;
-            if (useMultiplexing)
-            {
-                throw new NotImplementedException("Multiplex client is not implemented yet");
-            }
         }
 
         private BitmexSocketClientOptions(BitmexSocketClientOptions baseOn) : base(baseOn)
@@ -48,30 +61,10 @@ namespace Bitmex.Net.Client
         public static BitmexSocketClientOptions Default { get; set; } = new BitmexSocketClientOptions()
         {
         };
-        internal SocketApiClientOptions CommonStreamsOptions { get; set; }
+        public SocketApiClientOptions CommonStreamsOptions { get;}
         public bool IsTestnet { get; private set; }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="key"></param>
-        /// <param name="secret"></param>
-        /// <param name="isTestnet"></param>
-        /// <param name=""></param>
-        /// <param name="loadInstrumentIndexes">If you will subscribe to orderbook, set it to true, cause instrument index and tick size will be used for price calculation</param>
-        /// <param name="useMultiplexing"></param>
-        public BitmexSocketClientOptions(string key, string secret, bool isTestnet = false, bool loadInstrumentIndexes = true, bool useMultiplexing = false) : base()
-        {
-            LoadInstruments = loadInstrumentIndexes;
-            if (useMultiplexing)
-            {
-                throw new NotImplementedException("Multiplex client is not implemented yet");
-            }
-            IsTestnet = isTestnet;
-            key.ValidateNotNull(nameof(key));
-            secret.ValidateNotNull(nameof(secret));
-            ApiCredentials = new CryptoExchange.Net.Authentication.ApiCredentials(key, secret);
-        }
+
         public BitmexSocketClientOptions Copy()
         {
             return new BitmexSocketClientOptions(this);
